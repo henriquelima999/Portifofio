3eb18f1d532deed19eb788ca209ee526934ecd83
Henrique <henrique_lima1@hotmail.com>
2023-06-21 09:06:56 -0300

    adicionando arquivos

---
+++ script.js
@@ -0,0 +1,46 @@
+var divInf = document.getElementById('info')
+var divObj = document.getElementById('obj')
+var divForm = document.getElementById('form')
+var divQual = document.getElementById('qualif')
+var divExp = document.getElementById('exp')
+
+
+function abrirFecharInf(){
+    if(divInf.style.display === 'block'){
+        divInf.style.display = 'none'
+    }else{
+        divInf.style.display = 'block'
+    }
+}
+
+function abrirFecharObj(){
+    if(divObj.style.display === 'block'){
+        divObj.style.display = 'none'
+    }else{
+        divObj.style.display = 'block'
+    }
+}
+
+function abrirFecharForm(){
+    if(divForm.style.display === 'block'){
+        divForm.style.display = 'none'
+    }else{
+        divForm.style.display = 'block'
+    }
+}
+
+function abrirFecharQual(){
+    if(divQual.style.display === 'block'){
+        divQual.style.display = 'none'
+    }else{
+        divQual.style.display = 'block'
+    }
+}
+
+function abrirFecharExp(){
+    if(divExp.style.display === 'block'){
+        divExp.style.display = 'none'
+    }else{
+        divExp.style.display = 'block'
+    }
+}
